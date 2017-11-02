8667b729b86c4f46788419b6a821ec19c1d8e232
root <root@pingd1.honsec.com.tw>
2017-11-02 16:31:46 +0800

    test01

--- ping_ap.sh
+++ ping_ap.sh
@@ -7,10 +7,10 @@ while [ "${x}" != "8400" ]
 do
   str1=$(ping -c 1 -n -W 3 $HOST | grep 'time=' | awk '{ print $7 }')
   if test -z "$str1"; then
-    echo "$(date +'%Y%m%d %T') $HOST 9999.9" >> "/root/src/log/"$date1"_ap.log"
+    echo "$(date +'%Y%m%d %T') $HOST 9999.9" >> "/root/src/pingServer/log/"$date1"_ap.log"
   else
     str2="${str1#*time=}"
-    echo "$(date +'%Y%m%d %T') $HOST $str2" >> "/root/src/log/"$date1"_ap.log"
+    echo "$(date +'%Y%m%d %T') $HOST $str2" >> "/root/src/pingServer/log/"$date1"_ap.log"
     sleep 1
   fi
   x=$(($x+1))
