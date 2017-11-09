4fbc00c700c900d8779a72376ceaf9d826baea1b
root <root@pingl1.honsec.com.tw>
2017-11-09 09:46:01 +0800

    20171109 add by franco

---
+++ ping_sbb02.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+date1=$(date +%Y%m%d)
+x=1
+HOST="10.168.5.146"
+while [ "${x}" != "8400" ]
+do
+  str1=$(ping -c 1 -n -W 3 $HOST | grep 'time=' | awk '{ print $7 }')
+  if test -z "$str1"; then
+    echo "$(date +'%Y%m%d %T') $HOST 9999.9" >> "/root/src/pingServer/log/"$date1"_sbb02.log"
+  else
+    str2="${str1#*time=}"
+    echo "$(date +'%Y%m%d %T') $HOST $str2" >> "/root/src/pingServer/log/"$date1"_sbb02.log"
+  fi
+  sleep 1
+  x=$(($x+1))
+done
